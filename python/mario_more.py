0c9d7f3019b6b4aa59e143fb863b898f7415a29d
DustyHatz <dustyhatz21@mac.com>
2019-11-12 10:25:49 -0600

    add mario more program

---
+++ python/mario_more.py
@@ -0,0 +1,24 @@
+# This program builds a two sided pyramid to the height of the user input
+
+from cs50 import get_int
+
+
+# Create function for getting height from user
+
+
+def get_height(prompt):
+    while True:
+        n = get_int(prompt)
+        if n > 0 and n < 9:
+            return n
+
+
+# Get height from user
+height = get_height("Height: ")
+
+# Build pyramid
+for i in range(height):
+    print(' ' * (height - i - 1))
+    print('#' * (i + 1))
+    print('  ')
+    print('#' * (i + 1))
