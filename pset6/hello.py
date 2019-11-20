c5da9cf608316e71fc35ca26cc66b90eef80170d
DustyHatz <dustyhatz21@mac.com>
2019-11-20 16:08:11 -0600

    add hello.py

---
+++ pset6/hello.py
@@ -0,0 +1,7 @@
+# This program takes in a users name and says hello to that person!
+
+from cs50 import get_int, get_float, get_string
+
+name = get_string("What is your name?\n")
+
+print("hello, " + name)
